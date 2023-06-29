ed0ed95ad617e2aeec80bc3fe9cdf1b267f81df9
Chinyere Amasiatu <amasiatu@umich.edu>
2023-06-29 20:16:11 +0000

    Fixed PEP8 style errors in tests.py

--- tests.py
+++ tests.py
@@ -3,14 +3,20 @@ import unittest
 from unittest.mock import Mock, patch
 from name_finder import Names
 
+
 class TestNames(unittest.TestCase):
-    
+
     def setUp(self):
         self.name_finder = Names()
 
     def test_helper(self):
-        self.assertEqual(self.name_finder.get_name_gender_country('Chinyere'), ('Chinyere', 'Female', 'Nigeria'))
-        self.assertEqual(self.name_finder.get_name_gender_country('Phillipe'), ('Phillipe', 'Male', 'France'))
+        self.assertEqual(
+            self.name_finder.get_name_gender_country(
+                'Chinyere'), ('Chinyere', 'Female', 'Nigeria'))
+        self.assertEqual(
+            self.name_finder.get_name_gender_country(
+                'Phillipe'), ('Phillipe', 'Male', 'France'))
+
 
 if __name__ == '__main__':
     unittest.main()
