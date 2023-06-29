c46de8ef9f715491751d70f94b50425ea6e6dc9f
Chinyere Amasiatu <amasiatu@umich.edu>
2023-06-29 20:11:55 +0000

    Created functions to test Names class

--- tests.py
+++ tests.py
@@ -1,24 +1,16 @@
+import builtins
 import unittest
 from unittest.mock import Mock, patch
 from name_finder import Names
 
 class TestNames(unittest.TestCase):
-    # def test_start_program(self):
     
-    def test_find_origin(self):
-        # Create a mock object to simulate the user input
-        input_mock = Mock()
-        #input_mock.return_value = "The name Chinyere originates from Nigeria"
-        Names.find_origin(self)
-        #self.assertTrue(input_mock.called)
-        # Run the test case
-        input_mock.assert_called_with("Chinyere", "The name Chinyere originates from Nigeria")
-        #self.assertEqual(Names.find_origin(), input_mock.return_value)
+    def setUp(self):
+        self.name_finder = Names()
 
-    # def test_find_names(self):
-
-    # def test_baby_names(self):
-
+    def test_helper(self):
+        self.assertEqual(self.name_finder.get_name_gender_country('Chinyere'), ('Chinyere', 'Female', 'Nigeria'))
+        self.assertEqual(self.name_finder.get_name_gender_country('Phillipe'), ('Phillipe', 'Male', 'France'))
 
 if __name__ == '__main__':
     unittest.main()
